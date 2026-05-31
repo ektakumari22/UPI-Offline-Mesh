8d25176358058fcdb691c00edaa4c82e303fe748
Ekta Kumari <ektak9153@gmail.com>
2026-05-31 11:38:45 +0530

    Initial commit - UPI Offline Mesh

---
+++ src/main/java/com/demo/upimesh/UpiMeshApplication.java
@@ -0,0 +1,12 @@
+package com.demo.upimesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UpiMeshApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UpiMeshApplication.class, args);
+    }
+}
